c4d92fd296e52373f5a49ede33d51dd1b231bf12
Grivel-l <legrivel@student.le-101.fr>
2024-08-08 21:17:39 +0200

    Add some features + fix RGB when switching layer

--- neo/ergo/keymaps/default/keymap.c
+++ neo/ergo/keymaps/default/keymap.c
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define RGB_REPEAT_INTERVAL 80
+
 enum layers {
   DEFAULT,
   BASIC,
@@ -26,7 +28,9 @@ enum layers {
 };
 
 enum custom_keycodes {
-  TMUX_PREFIX = SAFE_RANGE
+  TMUX_PREFIX = SAFE_RANGE,
+  CUSTOM_COPY,
+  CUSTOM_PASTE
 };
 
 enum combo_events {
@@ -51,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(RAISE),  KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,   
         MO(COMMANDS), KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  
         KC_TRNS, KC_ESC, LSFT_T(KC_A),    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   RSFT_T(KC_SCLN), KC_QUOT,          KC_ENT,  
-        MO(LOWER),   TMUX_PREFIX, KC_NUBS, LCTL_T(KC_Z),    KC_X,    KC_C,   LALT_T(KC_V),     KC_B,    KC_N,    KC_N,    KC_M,   KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_UP,   MO(COMMANDS), 
+        MO(LOWER),   TMUX_PREFIX, KC_NUBS, LCTL_T(KC_Z),    KC_X,    KC_C,   LALT_T(KC_V),     KC_B,    QK_REPEAT_KEY,    KC_N,    KC_M,   KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_UP,   MO(COMMANDS), 
         KC_LCTL, MO(LOWER),                  KC_LGUI, KC_SPC,   MO(RAISE),  TMUX_PREFIX,                                    KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
     [RAISE] = LAYOUT_hot(
         KC_TRNS, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
-        KC_MPRV, KC_TRNS, KC_BTN3, KC_7, KC_8, KC_9, KC_A, KC_B, KC_EXCLAIM, KC_AT, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_EQUAL, KC_TRNS, KC_TRNS,
-        KC_MPLY, KC_TRNS, TMUX_PREFIX, KC_4, KC_5, KC_6, KC_C, KC_D, KC_HASH, KC_DOLLAR, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_CIRC,          KC_TRNS,
-        KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_E, KC_F, KC_TRNS, KC_MINUS, KC_ASTERISK, KC_AMPERSAND, KC_PERCENT, KC_BSLS, KC_TRNS,
+        KC_MPRV, CUSTOM_COPY, KC_BTN3, KC_7, KC_8, KC_9, KC_A, KC_B, KC_EXCLAIM, KC_AT, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_EQUAL, KC_TRNS, KC_TRNS,
+        KC_MPLY, CUSTOM_PASTE, TMUX_PREFIX, KC_4, KC_5, KC_6, KC_C, KC_D, KC_HASH, KC_DOLLAR, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_CIRC,          KC_TRNS,
+        KC_MNXT, KC_TRNS, KC_TRNS, KC_X, KC_1, KC_2, KC_3, KC_E, KC_F, KC_TRNS, KC_MINUS, KC_ASTERISK, KC_AMPERSAND, KC_PERCENT, KC_BSLS, KC_TRNS,
         KC_TRNS, KC_TRNS,                   KC_TRNS, KC_0, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
@@ -72,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [COMMANDS] = LAYOUT_hot(
-        LALT(KC_F10), KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, 
+        LALT(KC_F10), KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, QK_BOOTLOADER, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         TG(BASIC), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -117,29 +121,46 @@ void keyboard_post_init_user(void) {
     rgblight_setrgb_range(0, 0, 0, 0, 5);
 }
 
-void  turnLayersOff(void) {
-  rgblight_set_layer_state(2, false);
-  rgblight_set_layer_state(3, false);
-  rgblight_set_layer_state(4, false);
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint16_t keyTimer;
 
   switch (keycode) {
+    case CUSTOM_COPY:
+      if (record->event.pressed) {
+        register_code(KC_LEFT_CTRL);
+        register_code(KC_LEFT_SHIFT);
+        tap_code(KC_C);
+        unregister_code(KC_LEFT_CTRL);
+        unregister_code(KC_LEFT_SHIFT);
+      }
+      return false;
+    case CUSTOM_PASTE:
+      if (record->event.pressed) {
+        register_code(KC_LEFT_CTRL);
+        register_code(KC_LEFT_SHIFT);
+        tap_code(KC_V);
+        unregister_code(KC_LEFT_CTRL);
+        unregister_code(KC_LEFT_SHIFT);
+      }
+      return false;
     case TG(BASIC):
-      if (record->event.pressed)
-        rgblight_blink_layer_repeat(1, 80, 5);
+      // This is to fix an unwanted behavior with RGB when toggling layer
+      if (record->event.pressed) {
+        layer_off(COMMANDS);
+        rgblight_blink_layer_repeat(1, RGB_REPEAT_INTERVAL, 5);
+        layer_invert(BASIC);
+      }
+      return false;
     case LALT(KC_F10):
       if (record->event.pressed) {
-        turnLayersOff();
-        rgblight_blink_layer_repeat(0, 80, 5);
+        layer_off(COMMANDS);
+        rgblight_blink_layer_repeat(0, RGB_REPEAT_INTERVAL, 5);
       }
       break ;
     case TMUX_PREFIX:
       if (record->event.pressed)
         SEND_STRING(SS_LCTL("a"));
-      break;
+      return false;
     default:
       if (keyTimer != 0)
         keyTimer = 0;
