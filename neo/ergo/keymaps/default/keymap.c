025bd7c0c0300a9745de500651e5bee22a64377a
Grivel-l <legrivel@student.le-101.fr>
2024-08-26 17:28:55 +0200

    Replace useless right ctrl by left

--- neo/ergo/keymaps/default/keymap.c
+++ neo/ergo/keymaps/default/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(RAISE),  KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,   
         MO(COMMANDS), KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  
         KC_TRNS, KC_ESC, LSFT_T(KC_A),    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   RSFT_T(KC_SCLN), KC_QUOT,          KC_ENT,  
-        MO(LOWER),   TMUX_PREFIX, KC_NUBS, LCTL_T(KC_Z),    KC_X,    KC_C,   LALT_T(KC_V),     KC_B,    QK_REPEAT_KEY,    KC_N,    KC_M,   KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_UP,   MO(COMMANDS), 
+        MO(LOWER),   TMUX_PREFIX, KC_NUBS, LCTL_T(KC_Z),    KC_X,    KC_C,   LALT_T(KC_V),     KC_B,    QK_REPEAT_KEY,    KC_N,    KC_M,   KC_COMM, KC_DOT, LCTL_T(KC_SLSH), KC_UP,   MO(COMMANDS), 
         KC_LCTL, MO(LOWER),                  KC_LGUI, KC_SPC,   MO(RAISE),  TMUX_PREFIX,                                    KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(COMMANDS), KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  
         KC_TRNS, KC_ESC, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  
         MO(LOWER),   KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_LEFT_ALT,    KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_UP,   MO(COMMANDS), 
-        KC_LCTL, MO(LOWER),                  KC_LGUI, KC_SPC,   KC_ENT,  MO(RAISE),                                    KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTL, MO(LOWER),                  KC_LGUI, KC_SPC,   MO(RAISE),  KC_LEFT_ALT,                                    KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
 };
